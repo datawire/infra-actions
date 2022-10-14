28a369aa4451be84ef4ebe653aa0fdf32b9ecf93
Andres Osorio <aosorio@datawire.io>
2022-10-14 17:09:50 -0400

    Delete a Kubeception kluster

--- provision-cluster/delete.js
+++ provision-cluster/delete.js
@@ -1,14 +1,28 @@
+'use strict';
 const core = require('@actions/core');
 const github = require('@actions/github');
+const kubeception = require('./kubeception.js');
 
 try {
   // inputs are defined in action metadata file
   const distribution = core.getInput('distribution');
   const version = core.getInput('version');
-  console.log(`Deleting ${distribution} ${version}!`);
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
+
+	switch(distribution) {
+	   case "Kubeception": {
+	      kubeception.deleteKluster("aosorio-test-kluster").then(
+	        console.log(`Deleting ${distribution} ${version}!`)
+	      );
+	      break;
+	   }
+	   default: {
+			  console.log(`Deleting ${distribution} ${version}!`);
+	      break;
+	   }
+	}
 } catch (error) {
   core.setFailed(error.message);
 }
