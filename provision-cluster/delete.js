4b2b38fd3cdf57dad1f0c723051c9871e8d85124
Andres Osorio <aosorio@datawire.io>
2022-10-14 17:09:50 -0400

    Delete a Kubeception kluster

--- provision-cluster/delete.js
+++ provision-cluster/delete.js
@@ -3,24 +3,34 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+const kubeception = require('./kubeception.js')
 const registry = require('./registry.js')
 
 async function do_delete() {
   // inputs are defined in action metadata file
   const distribution = core.getInput('distribution')
-  let clusterName = core.getState(registry.CLUSTER_NAME)
 
-  let provider = registry.getProvider(distribution)
+  switch(distribution.toLowerCase()) {
+  case "kubeception":
+    kubeception.deleteKluster("aosorio-test-kluster").then(
+      console.log(`Deleting ${distribution} ${version}!`)
+    )
+    break
+  default:
+    let clusterName = core.getState(registry.CLUSTER_NAME)
 
-  let promises = []
-  promises.push(expire(provider))
+    let provider = registry.getProvider(distribution)
 
-  if (typeof clusterName !== typeof undefined && clusterName !== "") {
-    core.notice(`Deleting ${distribution} cluster ${clusterName}!`)
-    promises.push(delete_allocated(provider, clusterName))
+    let promises = []
+    promises.push(expire(provider))
+
+    if (typeof clusterName !== typeof undefined && clusterName !== "") {
+      core.notice(`Deleting ${distribution} cluster ${clusterName}!`)
+      promises.push(delete_allocated(provider, clusterName))
+    }
+
+    return Promise.all(promises)
   }
-
-  return Promise.all(promises)
 }
 
 async function expire(provider) {
