609d9631343ff2e5d97f6feae4135591e9da1d36
Andres Osorio <aosorio@datawire.io>
2022-10-17 09:51:42 -0400

    Refactor kubeception.ks module

--- provision-cluster/lib/kubeception.js
+++ provision-cluster/lib/kubeception.js
@@ -3,15 +3,16 @@ const core = require('@actions/core');
 const httpClient = require('@actions/http-client');
 const httpClientLib = require('@actions/http-client/lib/auth.js');
 
-const userAgent = 'datawire/provision-cluster'
+function getHttpClient() {
+	const userAgent = 'datawire/provision-cluster'
 
-function getCredentialHandler() {
   const kubeceptionToken = core.getInput('kubeceptionToken');
   if (!kubeceptionToken) {
     throw Error(`kubeceptionToken is missing. Make sure that input parameter kubeceptionToken was provided`);
   }
 
-	return new httpClientLib.BearerCredentialHandler(kubeceptionToken);
+	const credentialHandler = new httpClientLib.BearerCredentialHandler(kubeceptionToken);
+  return new httpClient.HttpClient(userAgent, [credentialHandler]);
 }
 
 async function createKluster(name, version) {
@@ -20,10 +21,9 @@ async function createKluster(name, version) {
     throw Error(`kubeceptionToken is missing. Make sure that input parameter kubeceptionToken was provided`);
   }
 
-	const credentialHandler = getCredentialHandler();
-  const client = new httpClient.HttpClient(userAgent, [credentialHandler]);
-	const oneDay = 86400
+  const client = getHttpClient();
 
+	const oneDay = 86400
 	const response = await client.put(`https://sw.bakerstreet.io/kubeception/api/klusters/${name}?version=${version}&wait=true&timeoutSecs=${oneDay}`);
 	if (!response || !response.message) {
 		throw Error("Unknown error getting response");
@@ -39,8 +39,7 @@ async function createKluster(name, version) {
 };
 
 async function deleteKluster(name) {
-	const credentialHandler = getCredentialHandler();
-  const client = new httpClient.HttpClient(userAgent, [credentialHandler]);
+  const client = getHttpClient();
 
 	const response = await client.del(`https://sw.bakerstreet.io/kubeception/api/klusters/${name}`);
 	if (!response || !response.message) {
