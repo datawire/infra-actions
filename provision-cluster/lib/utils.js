9d065d5648e4b1247105950fce51c7015133e081
Andres Osorio <aosorio@datawire.io>
2022-10-17 09:41:50 -0400

    Prefix kluster names with `test-`

--- provision-cluster/lib/utils.js
+++ provision-cluster/lib/utils.js
@@ -6,7 +6,7 @@ function getUniqueClusterName() {
   const sha = process.env['GITHUB_SHA'].substring(0, 8);
 	const uuid = uuidv4().replace('-', '').substring(0, 8);
 
-  let name = `${uuid}-${repoName}-${sha}-${branch}`;
+  let name = `test-${uuid}-${repoName}-${sha}-${branch}`;
   let sanitizedName = name.replace(/[^A-Za-z0-9-]/g, '-').toLowerCase().substring(0, 63);
 
 	return sanitizedName;
