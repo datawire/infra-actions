436ced9a61c29dd99f6756e0622c5147e10b09e3
Andres Osorio <aosorio@datawire.io>
2022-10-20 13:10:32 -0400

    Make cluster name length configurable in getUniqueClusterName

--- provision-cluster/lib/utils.js
+++ provision-cluster/lib/utils.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const crypto = require('crypto')
 const core = require('@actions/core')
 
-function getUniqueClusterName() {
+function getUniqueClusterName(maxNameLength) {
   const repoName = process.env['GITHUB_REPOSITORY'].replace(/^.*\//, '');
   const branch = process.env['GITHUB_HEAD_REF'];
   const sha = process.env['GITHUB_SHA'].substring(0, 8);
 
   let name = `ci-${uniqueId()}-${repoName}-${sha}-${branch}`;
-  let sanitizedName = name.replace(/[^A-Za-z0-9-]/g, '-').replace(/-+$/g, '').toLowerCase().substring(0, 63);
+  let sanitizedName = name.replace(/[^A-Za-z0-9-]/g, '-').replace(/-+$/g, '').toLowerCase().substring(0, maxNameLength);
 
 	return sanitizedName;
 }
