c5be8fcb2719e93ae28f140b5af61fc0365d80f5
Aidan Wenzel <aidanwenzel@gmail.com>
2022-10-25 17:34:27 -0700

    Quiet unzip

--- provision-runners/setup-runner.sh
+++ provision-runners/setup-runner.sh
@@ -11,7 +11,7 @@ set -e
 sudo apt -qq update
 sudo apt -qq install jq curl unzip moreutils -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -qq awscliv2.zip
 ./aws/install &> /dev/null
 
 #acquire runner token
