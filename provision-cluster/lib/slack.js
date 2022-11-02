04f5b2bcfd29b15930d1ad3f07fa7c2065b6353f
Rafael Schloming <rhs@datawire.io>
2022-11-02 16:32:17 -0400

    Add (but don't use yet) a slack notify function.

---
+++ provision-cluster/lib/slack.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const core = require('@actions/core')
+const http = require('@actions/http-client')
+
+async function notify(message) {
+  const slackWebhook = core.getInput('slackWebhook')
+  if (typeof slackWebhook === typeof undefined || slackWebhook === "") {
+    return
+  }
+
+  const channel = core.getInput('slackChannel')
+  const runbook = core.getInput('slackRunbook')
+  const username = core.getInput('slackUsername')
+
+  const client = new http.HttpClient('datawire/provision-cluster')
+
+  const body = {
+    channel: channel,
+    username: username,
+    text: `${message}\n\nSee runbook: ${runbook}`,
+    icon_emoji: `:kubernetes:`
+  }
+
+  const result = await client.postJson(slackWebhook, body)
+  if (result.statusCode != 200) {
+    throw new Error(`Status ${result.statusCode} posting to slack: ${result}`)
+  }
+}
+
+module.exports = {
+  notify
+}
