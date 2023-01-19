2d5764da35819a16b6c9e73db27ce562b736b7cc
Andres Osorio <aosorio@datawire.io>
2023-01-19 15:25:10 -0500

    Added prometheus counter to keep track or errors creating runners

--- github-runner-provisioner/internal/monitoring/prometheus.go
+++ github-runner-provisioner/internal/monitoring/prometheus.go
@@ -9,3 +9,8 @@ var ActionRunnerRuntime = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Subsystem: "action_runner",
 	Name:      "runtime",
 	Help:      "How long has an action runner been up."}, []string{"label", "instance_id"})
+
+var RunnerErrors = promauto.NewCounterVec(prometheus.CounterOpts{
+	Subsystem: "action_runner",
+	Name:      "errors",
+	Help:      "Errors managing runners on AWS."}, []string{"error", "runner_label"})
