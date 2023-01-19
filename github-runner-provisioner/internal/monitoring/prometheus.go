5593a44bc55b7b6efbd6fa9b4cfb209b64f37bca
Andres Osorio <aosorio@datawire.io>
2023-01-19 16:14:35 -0500

    Define provisioning errors under the monitoring package

--- github-runner-provisioner/internal/monitoring/prometheus.go
+++ github-runner-provisioner/internal/monitoring/prometheus.go
@@ -5,6 +5,35 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+type ProvisioningError int
+
+const (
+	ErrorBadRequest ProvisioningError = iota
+	ErrorInvalidAuthentication
+	ErrorInvalidPayload
+	ErrorUnknownAction
+	ErrorUnknownRunnerLabel
+	ErrorRunnerCreation
+)
+
+func (s ProvisioningError) String() string {
+	switch s {
+	case ErrorBadRequest:
+		return "bad_request"
+	case ErrorInvalidAuthentication:
+		return "authentication_error"
+	case ErrorInvalidPayload:
+		return "invalid_request"
+	case ErrorUnknownAction:
+		return "unknown_workflow_action"
+	case ErrorUnknownRunnerLabel:
+		return "unknown_runner_label"
+	case ErrorRunnerCreation:
+		return "runner_creation_error"
+	}
+	return "unknown_error"
+}
+
 var ActionRunnerRuntime = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Subsystem: "action_runner",
 	Name:      "runtime",
